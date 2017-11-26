476bb4944b08781d3f2527add6d928949337d6b5
lslgg <449291161@qq.com>
2017-11-26 14:46:25 +0800

    ok

--- src/power/power.ts
+++ src/power/power.ts
@@ -1,37 +1,38 @@
 import { model, Schema, Document } from 'mongoose';
 
 export interface IPowerModel extends Document {
-    title:String
-    operation:[String]
-    code:String
-    url:String
-    explain:String 
-    type:String  
-    isValid:Boolean
-    updateAt:Date
-    createAt:Date
+	title: String;
+	operation: [String];
+	code: String;
+	url: String;
+	explain: String;
+	type: String;
+	isValid: Boolean;
+	updateAt: Date;
+	createAt: Date;
 }
-const GENDERS=['SHOW','ADD','UPDATE','DELETE']
+const OPERATION = [ 'SHOW', 'ADD', 'UPDATE', 'DELETE' ];
 let schema: Schema = new Schema({
-    title:String,
-    operation:{
-        type: [String], enum: GENDERS
-    },
-    code:String,
-    url:String,
-    explain:String, 
-    type:String,  
-    isValid:Boolean,
-    createAt: {
-        type: Date,
-        default: new Date(),
-        required: true
-    },
-    updateAt: {
-        type: Date,
-        default: new Date(),
-        required: true
-    },
-})
+	title: String,
+	operation: {
+		type: [ String ],
+		enum: OPERATION
+	},
+	code: String,
+	url: String,
+	explain: String,
+	type: String,
+	isValid: Boolean,
+	createAt: {
+		type: Date,
+		default: new Date(),
+		required: true
+	},
+	updateAt: {
+		type: Date,
+		default: new Date(),
+		required: true
+	}
+});
 
 export default model<IPowerModel>('Power', schema);
