5c14ecc1b388772257af9518099467473fcee244
resdf123123 <4370320704@qq.com>
2018-01-10 16:13:13 +0800

    ok

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ import App from './server';
 
 App.listen(8080, function () {
 	console.log('Now browse to http://localhost:8080/playground');
+	console.log('Now browse to http://localhost:8080/playground');
 });
 
